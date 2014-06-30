fad3252282ef2ab9dcfcd8632a1e0f5279b3c217
Benjamin Borbe <bborbe@rocketnews.de>
2014-06-30 16:21:22 +0200

    add sample

---
+++ src/main/java/example/GeoUtil.java
@@ -0,0 +1,13 @@
+package example;
+
+public class GeoUtil {
+
+	public double distance(double x_a, double y_a, double x_b, double y_b) {
+		return 0;
+	}
+
+	public double area(double x_a, double y_a, double x_b, double y_b, double x_c, double y_c) {
+		return 0;
+	}
+
+}
