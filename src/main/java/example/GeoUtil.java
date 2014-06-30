d28d670648422898e379d5b769e4409fa300546f
Benjamin Borbe <bborbe@rocketnews.de>
2014-06-30 16:28:55 +0200

    add sample

--- src/main/java/example/GeoUtil.java
+++ src/main/java/example/GeoUtil.java
@@ -2,12 +2,12 @@ package example;
 
 public class GeoUtil {
 
-	public double distance(double x_a, double y_a, double x_b, double y_b) {
-		return 0;
-	}
+    public double distance(double x_a, double y_a, double x_b, double y_b) {
+        return x_a + x_b + y_a + y_b;
+    }
 
-	public double area(double x_a, double y_a, double x_b, double y_b, double x_c, double y_c) {
-		return 0;
-	}
+    public double area(double x_a, double y_a, double x_b, double y_b, double x_c, double y_c) {
+        return x_a + x_b + y_a + y_b + y_c + x_c;
+    }
 
 }
